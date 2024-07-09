501dbaff4a0cc23a2c167256064e9cd0db7384c7
Chaz Schooler <schooler.charles@gmail.com>
2024-07-09 10:46:16 -0400

    feat: storage class for external drive

---
+++ storage/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.9.1"
+
+  backend "kubernetes" {
+    secret_suffix = "storage"
+    config_path   = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_storage_class_v1" "local_path" {
+  metadata {
+    name = "ext-storage"
+  }
+  storage_provisioner = "rancher.io/local-path"
+  parameters = {
+    nodePath    = "/mnt/storage"
+    pathPattern = "{{ .PVC.Namespace }}/{{ .PVC.Name }}"
+  }
+  volume_binding_mode = "WaitForFirstConsumer"
+  reclaim_policy      = "Delete"
+}
+
