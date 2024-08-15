5931eb0f9bc260769ae1af2fc8b0280aa3f2690f
Chaz Schooler <schooler.charles@gmail.com>
2024-08-15 13:21:57 -0400

    feat: verdaccio github oauth configuration

--- npm/main.tf
+++ npm/main.tf
@@ -17,6 +17,10 @@ provider "helm" {
   }
 }
 
+locals {
+  plugins_path = "/opt/verdaccio/plugins"
+}
+
 resource "kubernetes_persistent_volume_claim_v1" "verdaccio" {
   metadata {
     name = "verdaccio-pvc"
@@ -27,6 +31,24 @@ resource "kubernetes_persistent_volume_claim_v1" "verdaccio" {
     resources {
       requests = {
         storage = "16Gi"
+      }
+    }
+  }
+  # local-path storage class uses WaitForFirstConsumer
+  wait_until_bound = false
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "verdaccio_plugins" {
+  metadata {
+    name = "verdaccio-plugins-pvc"
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    storage_class_name = "local-path"
+    resources {
+      requests = {
+        storage = "8Gi"
       }
     }
   }
@@ -46,5 +68,84 @@ resource "helm_release" "verdaccio" {
     name  = "persistence.existingClaim"
     value = kubernetes_persistent_volume_claim_v1.verdaccio.metadata[0].name
   }
+
+  set {
+    name  = "existingConfigMap"
+    value = kubernetes_config_map_v1.verdaccio_config.metadata[0].name
+  }
+
+  set {
+    name = "persistence.mounts"
+    value = yamlencode([
+      {
+        mountPath = local.plugins_path
+        name      = "plugins"
+      }
+    ])
+  }
+
+  set {
+    name = "persistence.volumes"
+    value = yamlencode([
+      {
+        name = "plugins"
+        persistentVolumeClaim = {
+          claimName = kubernetes_persistent_volume_claim_v1.verdaccio_plugins.metadata[0].name
+        }
+      }
+    ])
+  }
 }
 
+data "kubernetes_secret_v1" "verdaccio_github_oauth" {
+  metadata { name = "verdaccio-secrets" }
+  binary_data = {
+    github_client_id     = ""
+    github_client_secret = ""
+    github_token         = ""
+  }
+}
+
+resource "kubernetes_config_map_v1" "verdaccio_config" {
+  metadata {
+    name = "verdaccio-config"
+  }
+  data = {
+    "config.yaml" = yamlencode({
+      auth = {
+        "github-oauth-ui" = {
+          "client-id" = base64decode(data.kubernetes_secret_v1.verdaccio_github_oauth.binary_data.github_client_id)
+          "client-secret" = base64decode(data.kubernetes_secret_v1.verdaccio_github_oauth.binary_data.github_client_secret)
+          "token" = base64decode(data.kubernetes_secret_v1.verdaccio_github_oauth.binary_data.github_token)
+        }
+      }
+      middlewares = {
+        "github-oauth-ui" = {
+          enabled = true
+        }
+      }
+      packages = {
+        "**" = {
+          access = "github/user/schoolerc"
+          publish = "github/user/schoolerc"
+        }
+      }
+      plugins = "${local.plugins_path}/node_modules"
+      security = {
+        api = {
+          jwt = {
+            sign = {
+              expiresIn = "7d"
+            }
+          }
+        }
+      }
+      storage = "./storage"
+      uplinks = {
+        npm = {
+          url = "https://registry.npmjs.org/"
+        }
+      }
+    })
+  }
+}
